8a98ac2e368d5de12f8aa789beff9f7821030340
Paul Calot <paul.calot@hotmail.fr>
2022-12-01 19:36:19 +0100

    correct wrong imports in __init__ files

--- pynance/model/__init__.py
+++ pynance/model/__init__.py
@@ -1,7 +1,3 @@
 from pynance.model import forecasting
-from pynance.model import lm
-from pynance.model import loss
 from pynance.model import metrics
 from pynance.model import regression
-from pynance.model import train
-from pynance.model import utils
