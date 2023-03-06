1e9cccf1cbafcb05bc0d0fef96652f3308f18eba
Paul Calot <paul.calot@hotmail.fr>
2023-03-06 15:55:53 +0100

    Remove unused -s param

--- run.py
+++ run.py
@@ -10,14 +10,14 @@ from pynance.utils.user import get_path_to_config_files
 
 def print_usage():
     print('Usage : ')
-    print('run.py -p <cfg_name> -s')
+    print('run.py -n <cfg_name>')
     print('`.cfg` is automatically added to the name')
     print(f'File <cfg_name>.cfg should exist in folder {get_path_to_config_files()}')
     print('To change it, modify `USERCFG` file.')
 
 def run(argv):
     try:
-        opts, args = getopt.getopt(argv,"hp:s", ['path=']) # h for help, s for save
+        opts, args = getopt.getopt(argv,"hn:", ['path=']) # h for help, s for save
     except getopt.GetoptError:
         print_usage()
         sys.exit(2)
@@ -29,15 +29,11 @@ def run(argv):
     if(opts[0][0] == '-h'):
         print_usage()
         sys.exit()
-    elif opts[0][0] in ('-p','--path'):
+    elif opts[0][0] in ('-n','--name'):
         cfg_name = opts[0][1]
         cfg_path = get_path_to_config_files() / f'{cfg_name}.cfg'
-        print(cfg_path)
-        if(opts[1][0] in ('-s','-save')):
-            main(cfg_path, save = True)
-        else :
-            main(cfg_path, save = False)
-
+        print(f'Config file used: {cfg_path}')
+        main(cfg_path, save=True)
     else :
         print('Please precise path to cfg.')
         print_usage()
