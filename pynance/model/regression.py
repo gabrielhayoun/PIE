93d307d47674913aec4d627dcb4acf3047e0d3e5
Paul Calot <paul.calot@hotmail.fr>
2023-03-06 15:28:38 +0100

    add models saving

--- pynance/model/regression.py
+++ pynance/model/regression.py
@@ -3,57 +3,56 @@ import torch
 import sklearn
 from sklearn import linear_model
 import numpy as np
+import joblib
+import re
 
-# local
-import pynance
-
-class StockRegressionPredictionPytorch(torch.nn.Module):
-    def __init__(self, input_size):
-        # simple regression model for now
-        self.input_size = input_size
-        self.linear = torch.nn.Linear(input_size, out_features=1, bias=True)
+# class StockRegressionPredictionPytorch(torch.nn.Module):
+#     def __init__(self, input_size):
+#         # simple regression model for now
+#         self.input_size = input_size
+#         self.linear = torch.nn.Linear(input_size, out_features=1, bias=True)
         
-    def forward(self, X):
-        # X : batch size x input size
-        X = self.linear(X)
-        return X
+#     def forward(self, X):
+#         # X : batch size x input size
+#         X = self.linear(X)
+#         return X
     
-    def predict(self, X):
-        self.test()
-        with torch.no_grad():
-            preds = self.forward(X)
-        self.train()
-        return preds
+#     def predict(self, X):
+#         self.test()
+#         with torch.no_grad():
+#             preds = self.forward(X)
+#         self.train()
+#         return preds
 
-    def score(self, X, y): # Rsquared socre
-        preds = self.predict(X)
-        return pynance.model.metrics.r_squared(y, preds)
+#     def score(self, X, y): # Rsquared socre
+#         preds = self.predict(X)
+#         return pynance.model.metrics.r_squared(y, preds)
 
+# class StockRegressionPredictionSklearn:
+#     # https://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessRegressor.html
 
-class StockRegressionPredictionSklearn:
-    # https://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessRegressor.html
+#     def __init__(self, input_size, random_state=0) -> None:
+#         self.input_size = input_size
+#         self.kernel = sklearn.gaussian_process.kernels.DotProduct() + (
+#                         sklearn.gaussian_process.kernels.WhiteKernel())
+#         self.gpr = sklearn.gaussian_process.GaussianProcessRegressor(
+#                             kernel=self.kernel,
+#                             random_state=random_state,
+#                             n_features_in_=self.input_size)
 
-    def __init__(self, input_size, random_state=0) -> None:
-        self.input_size = input_size
-        self.kernel = sklearn.gaussian_process.kernels.DotProduct() + (
-                        sklearn.gaussian_process.kernels.WhiteKernel())
-        self.gpr = sklearn.gaussian_process.GaussianProcessRegressor(
-                            kernel=self.kernel,
-                            random_state=random_state,
-                            n_features_in_=self.input_size)
+#     def fit(self, X, y):
+#         self.gpr.fit(X, y)
 
-    def fit(self, X, y):
-        self.gpr.fit(X, y)
-
-    def predict(self, X):
-        y_mean, y_std = self.gpr.predict(X, return_std=True)
-        return y_mean, y_std
+#     def predict(self, X):
+#         y_mean, y_std = self.gpr.predict(X, return_std=True)
+#         return y_mean, y_std
     
-    def score(self, X, y):
-        score = self.gpr.score(X, y)
-        return score
+#     def score(self, X, y):
+#         score = self.gpr.score(X, y)
+#         return score
 
 class MultipleLinearRegression:
+    pattern = 'mlr_reg:[0-9]+.bin'
     def __init__(self, number_of_submodels) -> None:
         self.number_of_submodels = number_of_submodels
         self.submodels = [sklearn.linear_model.LinearRegression() for k in range(number_of_submodels)]
@@ -77,3 +76,27 @@ class MultipleLinearRegression:
             score = reg.score(x_transpose, y_transpose[:, i, :])
             scores.append(score)
         return scores
+    
+    def save(self, saving_dir):
+        for i, reg in enumerate(self.submodels):
+            joblib.dump(reg, saving_dir / f'mlr_reg:{i}.bin')
+
+    def load(self, path_dir):
+        paths = path_dir.glob('*.bin')
+        submodels = {}
+        for p in paths:
+            stem = p.stem
+            if(re.match(stem, self.pattern)):
+                index = int(stem.split(':')[1]) # get number
+                submodels[index] = joblib.load(p)
+        list_of_submodels = [None] * len(submodels)
+        for key, value in submodels.items():
+            try:
+                list_of_submodels[key] = value
+            except Exception as e:
+                raise e
+        if(None not in self.submodels): # second security, in theory useless
+            self.submodels = list_of_submodels
+            return True
+        else:
+            return False
