correct error in size computation

pynance/utils/datasets/torch.py
@@ -4,7 +4,7 @@ class SlidingWindowDataset(torch.utils.data.Dataset):
     # TODO: can we make it faster ? Don't remember how __getitem__ works...
     def __init__(self, data, window):
         self.data = data # number of featues x time series length
-        self.window = min(window, len(self.data)-1)
+        self.window = min(window, data.shape[1]-1)
 
     def __getitem__(self, index):
         x = self.data[:, index: index + self.window]