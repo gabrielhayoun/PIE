87aba6cf40b23968828e2757169d6cef51a7f846
Paul Calot <paul.calot@hotmail.fr>
2023-03-07 13:53:55 +0100

    Add kind arg to run script

--- run.py
+++ run.py
@@ -5,19 +5,27 @@ import sys, getopt
 from pathlib import Path
 
 # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
-from pynance import train
-from pynance.utils.user import get_path_to_config_files
+import pynance
 
 def print_usage():
     print('Usage : ')
-    print('run.py -n <cfg_name>')
+    print('run.py -n <cfg_name> -k <process_kind>')
     print('`.cfg` is automatically added to the name')
-    print(f'File <cfg_name>.cfg should exist in folder {get_path_to_config_files()}')
+    print(f'File <cfg_name>.cfg should exist in folder {pynance.utils.user.get_path_to_config_files()}')
     print('To change it, modify `USERCFG` file.')
+    print('Available kind process: train, infer, crypto.')
+
+def get_function_from_kind(kind):
+    switch = {
+        'train': pynance.train.main,
+        'infer': pynance.infer.main,
+        # 'crypto': pynance.crypto.main
+    }
+    return switch.get(kind, "Invalid input")
 
 def run(argv):
     try:
-        opts, args = getopt.getopt(argv,"hn:", ['path=']) # h for help, s for save
+        opts, args = getopt.getopt(argv,"hn:k:", ['path=']) # h for help, s for save
     except getopt.GetoptError:
         print_usage()
         sys.exit(2)
@@ -31,9 +39,13 @@ def run(argv):
         sys.exit()
     elif opts[0][0] in ('-n','--name'):
         cfg_name = opts[0][1]
-        cfg_path = get_path_to_config_files() / f'{cfg_name}.cfg'
+        cfg_path = pynance.utils.user.get_path_to_config_files() / f'{cfg_name}.cfg'
         print(f'Config file used: {cfg_path}')
-        train.main(cfg_path)
+        if(opts[1][0] in ('-k', '--kind')):
+            kind = opts[1][1]
+            print(f'Process kind: {kind}')
+            main_fn = get_function_from_kind(kind)
+            main_fn(cfg_path)
     else :
         print('Please precise path to cfg.')
         print_usage()
