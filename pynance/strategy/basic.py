d17aa682899df0849ec59d2d28a3d30422141cdf
Paul Calot <paul.calot@hotmail.fr>
2023-03-10 12:59:13 +0100

    Correct bugs

--- pynance/strategy/basic.py
+++ pynance/strategy/basic.py
@@ -3,13 +3,12 @@ import numpy as np
 # S1 et S2 sont les prévisions des deux actions co-intégrés étudiées et dont on a effectué une prévision
 # La fonction performance permet de connaître les gains potentiels sur les simulations effectués en travaillant sur la fonction ratio des paires d'action co-intégrées
 # r fait référence au risque qu'on est prêt à prendre. Par exemple pour r = 0.1, on achètera jamais plus de r*money/S1 du produit 1.
-def performance(S1, S2, risk, money):
+def get_performance(S1, S2, risk, money):
     assert(type(S1) == np.ndarray)
     assert(type(S2) == np.ndarray)
     ratios = S1/S2
     zscore = (ratios - ratios.mean()) / np.std(ratios) 
     # On peut à priori commencer à trader avec rien puisqu'on trade quand le ratio vaut 1 ou -1
-    money = money
     countS1 = 0
     countS2 = 0
     # TODO: we should be able to vectorize it completely.
@@ -29,7 +28,7 @@ def performance(S1, S2, risk, money):
             money += S1[i] * countS1 + S2[i] * countS2
             countS1 = 0
             countS2 = 0          
-    return money # On retourne le gain potentiel 
+    return float(money) # On retourne le gain potentiel 
 
 # On veut permettre au client de choisir le nombre de pair sur lesquelles il va trader.
 # On lui permet de  peut choisir la p-value minimale afin de sélectionner ses paires
@@ -38,7 +37,45 @@ def performance(S1, S2, risk, money):
 # paires ayant le meilleur gain potentiel et false si on veut trader sur toutes les paires ayant une p_value minimal)
 # En sorti : gain total théorique de la stratégie (+ perfo de chaque pair) / graphe prévision + ordre d'achat sur les paires sélectionnées
 # TODO: improve clarity (better naming) - and maybe also performance while doing so (use dataframe, vectorized operations) - on this function
-def best_action(dico1, data, nb_pair, p_value_limite, money, risk, risque=False):
+def get_pairs(names):
+    names.sort() # inplace I believe
+    pairs = []
+    for i in range(len(names)):
+        for j in range(i+1, len(names)):
+            pairs.append((names[i], names[j]))
+    return pairs
+
+def get_best_action(df_coint, data_dicts, p_value_limit, money, risk, nb_pairs, sort_by_risk=True):
+    df_coint_ = df_coint.copy(deep=True)
+    names = list(data_dicts.keys())
+    pairs = get_pairs(names)
+    df_coint_ = df_coint_[df_coint_['ticker1'].isin(names) & df_coint_['ticker2'].isin(names)]
+    assert len(df_coint_) == len(pairs), f'Pairs length: {len(pairs)} but {len(df_coint_)} pairs in cointegration dataframe.'
+    print(f'Number of pairs to study in data: {len(df_coint_)}')
+    # we can either sort by risk (~p-value) or by performance
+    print(f'Selection of co-integrated pairs (p-value < {p_value_limit}).')
+    df_coint_ = df_coint_[df_coint_['p-value'] < p_value_limit]
+
+    df_coint_['performance'] = [
+        get_performance(data_dicts[row['ticker1']], data_dicts[row['ticker2']], risk, money) for i, row in
+        df_coint_.iterrows()]
+
+    if sort_by_risk: 
+        print('Sorting by p-value (~risk).')
+        df_coint_.sort_values(by='p-value', inplace=True)
+    else :    
+        print('Sorting by performance.')
+        df_coint_.sort_values(by='performance', inplace=True)
+
+    print(f'Selection of best {nb_pairs} pairs.')
+    df_coint_ = df_coint_.head(nb_pairs)
+
+    print(f'Theoretical gain for {money} money (per pair).')
+    print(df_coint_[['ticker1', 'ticker2', 'performance']])
+    print("Total gain :", df_coint_['performance'].sum())
+    return df_coint_
+
+def best_action_old(dico1, data, nb_pair, p_value_limite, money, risk, risque=False):
     dico2 = {}
     # On parcourt dico1 pour récupérer les paires dont les p_value sont inférieures à p_value_limite
     for key, value in dico1.items():
@@ -47,14 +84,14 @@ def best_action(dico1, data, nb_pair, p_value_limite, money, risk, risque=False)
     if risque : 
         dico3 = {}
         for key, value in dico2.items():
-            dico3[key] = performance(data[key[0]], data[key[1]], risk, money)
+            dico3[key] = get_performance(data[key[0]], data[key[1]], risk, money)
         # On trie dico3 par ordre décroissant de performance
         dico3 = sorted(dico3.items(), key=lambda x: x[1], reverse = True)
         # On récupère les nb_pair premières paires
         dico3 = dico3[:nb_pair]
         gain = []
         for i in range(len(dico3)):
-            gain.append(performance(data[dico3[i][0][0]], data[dico3[i][0][1], risk, money]))
+            gain.append(get_performance(data[dico3[i][0][0]], data[dico3[i][0][1], risk, money]))
     else :    
         # On trie dico2 par ordre croissant de p_value
         dico2 = sorted(dico2.items(), key=lambda x: x[1])
